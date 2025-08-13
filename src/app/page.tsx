061becfd7d5862cf40ce64fc5de991966ebb6483
Jonathan <jonathancalderonsilberman@gmail.com>
2025-08-13 03:39:24 -0700

    linter fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,13 @@
-import Link from "next/link";
-
-import { api, HydrateClient } from "~/trpc/server";
+import { HydrateClient } from "~/trpc/server";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
-
-  void api.post.getLatest.prefetch();
 
   return (
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            Hi, I'm Jonathan
+            Hi, I&apos;m Jonathan
           </h1>
         </div>
       </main>
